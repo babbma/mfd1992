b1dbd59eccf54491cb1c7e01850dee8642e5a1b6
babbma <cn.chunhua.zhang@qq.com>
2016-01-08 14:27:00 +0800

    修改测试类 pom输出 输入编码设置

--- admin/src/test/java/admin/SpringTest.java
+++ admin/src/test/java/admin/SpringTest.java
@@ -1,14 +1,28 @@
 package admin;
 
-import static org.junit.Assert.fail;
+import java.util.HashMap;
+
+import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.app.dao.UserDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional(value="transactionManager")
+//@WebAppConfiguration
+@ContextConfiguration(value={"classpath:spring/application-config.xml"})
 public class SpringTest {
+	@Resource
+	private UserDao userDao;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		userDao.findByList(new HashMap<String, Object>());
 	}
 
 }
